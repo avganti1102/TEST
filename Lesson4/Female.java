f470d7c05dd7e5f2ae158b22fabb2d7c5b0c3f06
avganti1102 <avganti1001@gmail.com>
2020-09-19 09:49:36 +0700

    Buổi 4

---
+++ Lesson4/Female.java
@@ -0,0 +1,10 @@
+package Lesson4;
+
+import Lesson3.subPackage1.Person;
+
+public class Female {
+	public void female1(){
+		Person p = new Person();
+		p.name = "abc";
+	}
+}
---
+++ Lesson4/Male.java
@@ -0,0 +1,10 @@
+package Lesson4;
+
+import Lesson3.subPackage1.Person;
+
+public class Male extends Person{
+	public void male1(){
+		String s1 = name;
+		int s2 = id;
+	}
+}
